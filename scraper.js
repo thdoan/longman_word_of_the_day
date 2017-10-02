69e22933d759459a9567f7147f9f714e0569780c
Tom Doan <tom_xyz@yahoo.com>
2017-10-02 01:56:41 -0700

    Initial commit

--- scraper.js
+++ scraper.js
@@ -1,59 +1,55 @@
 // This is a template for a Node.js scraper on morph.io (https://morph.io)
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+var cheerio = require('cheerio');
+var request = require('request');
+var sqlite3 = require('sqlite3').verbose();
 
 function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+  // Set up sqlite database.
+  var db = new sqlite3.Database('data.sqlite');
+  db.serialize(function() {
+    db.run('CREATE TABLE IF NOT EXISTS data (word TEXT, definition TEXT)');
+    callback(db);
+  });
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
+function updateRow(db, values) {
+  // Insert some data.
+  var statement = db.prepare('INSERT INTO data VALUES(?,?)');
+  statement.run(values);
+  statement.finalize();
 }
 
 function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
+  // Read some data.
+  db.each('SELECT rowid as id, * FROM data', function(err, row) {
+    console.log(row);
+  });
 }
 
 function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
-
-		callback(body);
-	});
+  // Use request to read in pages.
+  request(url, function(error, response, body) {
+    if (error) {
+      console.log('Error requesting page: ' + error);
+      return;
+    }
+    callback(body);
+  });
 }
 
 function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
+  // Use request to read in pages.
+  fetchPage('http://www.ldoceonline.com', function(body) {
+    // Use cheerio to find things in the page with css selectors.
+    var $ = cheerio.load(body);
+    var values = [];
+    values.push(($('#wotd .title_entry a').text().trim())); // Word
+    values.push(($('#wotd .ldoceEntry a').text().trim()));  // Definition
+    updateRow(db, values);
+    //readRows(db);
+    db.close();
+  });
 }
 
 initDatabase(run);
